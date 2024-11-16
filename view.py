d1eb7f4d1f31aff623b7ee7c28f41a617fe0d34c
Miltondeveloper <miltoneng.computacao@gmail.com>
2024-11-16 18:04:09 -0300

    Adicionando o comando Delete

--- view.py
+++ view.py
@@ -12,10 +12,10 @@ lista= ['Terminar o PI', "08/12/2024", "11/24/2024", 'Alta', 'Projeto integrador
 
 #inserir informações
 
-#with conex:
-   # cur = conex.cursor()
-   # query = "INSERT INTO lista_de_tarefas (Nome_da_tarefa, Data_de_inicio, Data_de_Termino, Prioridade, Descrição) VALUES (?,?,?,?,?)"
-   # cur.execute(query, lista)
+with conex:
+    cur = conex.cursor()
+    query = "INSERT INTO lista_de_tarefas (Nome_da_tarefa, Data_de_inicio, Data_de_Termino, Prioridade, Descrição) VALUES (?,?,?,?,?)"
+    cur.execute(query, lista)
 
 
 # R = Read/Ler
@@ -29,10 +29,21 @@ with conex:
     print(info)
 
 
-# U = Update/atualizar
 
 lista = ['Concluir projeto integrador', 1]
+# U = Update/atualizar
+
 with conex:
     cur = conex.cursor()
     query = "UPDATE lista_de_tarefas SET Nome_da_tarefa=? WHERE id=?"
+    cur.execute(query, lista)
+
+
+
+lista = [1]
+# D = Delete/apagar
+
+with conex:
+    cur = conex.cursor()
+    query = "DELETE FROM lista_de_tarefas WHERE id=?"
     cur.execute(query, lista)
