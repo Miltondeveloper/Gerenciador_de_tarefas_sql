bb31f216d48eab9d75015c5e985459ed4a2f639d
Miltondeveloper <miltoneng.computacao@gmail.com>
2024-11-16 13:38:12 -0300

    Criando janela
    criando a janela e definindo a cor de fundo

---
+++ main.py
@@ -0,0 +1,24 @@
+# importando o custom tkinter
+from customtkinter import *
+
+################# cores ###############
+co0 = "#f0f3f5"  # Preta
+co1 = "#feffff"  # branca
+co2 = "#4fa882"  # verde
+co3 = "#38576b"  # valor
+co4 = "#403d3d"   # letra
+co5 = "#e06636"   # - profit
+co6 = "#038cfc"   # azul
+co7 = "#ef5350"   # vermelha
+co8 = "#263238"   # + verde
+co9 = "#e9edf5"   # sky blue
+
+# Criando janela
+
+janela = CTk()
+janela.title("")
+janela.geometry("800x500")
+janela.configure(background = co9)
+
+
+janela.mainloop()
